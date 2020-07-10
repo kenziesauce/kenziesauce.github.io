075154abce2886a538116f971ca0d285547cef4c
makenzie <kenzie@miadolce.com>
2020-07-10 11:38:38 -0400

    js

---
+++ js/contact.js
@@ -0,0 +1,12 @@
+﻿//index.js  
+function sendEmail() {
+	Email.send({
+	SecureToken : "c842cd55-489d-46d1-a5b3-9437f4006d9e",
+	To : 'kenzie@miadolce.com',
+	From : "<sender’s email address>",
+	Subject : "Test",
+	Body : "a Test",
+	}).then(
+		message => alert("Message Sent!")
+	);
+}
