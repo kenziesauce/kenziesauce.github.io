2c82e20c5f659581e68ccf738be93a4f8b81cc83
makenzie <kenzie@miadolce.com>
2020-07-10 13:59:46 -0400

    gah

--- js/contact.js
+++ js/contact.js
@@ -1,45 +1,26 @@
-$(function () {
-
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
-
-    $('#contact-form').validator();
-
-
-    // when the form is submitted
-    $('#contact-form').on('submit', function (e) {
-
-        // if the validator does not prevent form submit
-        if (!e.isDefaultPrevented()) {
-            var url = "contact.php";
-
-            // POST values in the background the the script URL
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    // data = JSON object that contact.php returns
-
-                    // we recieve the type of the message: success x danger and apply it to the 
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            return false;
-        }
-    })
+$(document).ready(function() {
+$("#submit").click(function() {
+var name = $("#contact-name").val();
+var email = $("#contact-email").val();
+var message = $("#contact-message").val();
+var contact = $("#contact-service").val();
+$("#returnmessage").empty(); // To empty previous error/success message.
+// Checking for blank fields.
+if (name == '' || email == '' || contact == '') {
+alert("Please Fill Required Fields");
+} else {
+// Returns successful data submission message when the entered information is stored in database.
+$.post("contact_form.php", {
+name1: name,
+email1: email,
+message1: message,
+contact1: contact
+}, function(data) {
+$("#returnmessage").append(data); // Append returned message to message paragraph.
+if (data == "Your Query has been received, We will contact you soon.") {
+$("#form")[0].reset(); // To reset form fields on success.
+}
+});
+}
+});
 });
