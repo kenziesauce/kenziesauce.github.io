80b3cb9cf4b0f48d1be17d626666d73a3cb38510
makenzie <kenzie@miadolce.com>
2020-07-10 11:55:26 -0400

    ???

--- js/contact.js
+++ js/contact.js
@@ -2,7 +2,7 @@
 function sendEmail() {
 	Email.send({
 	SecureToken : "772c5200-0b15-4a94-8ff4-f77afe9b0205",
-	To : 'kenzie@miadolce.com',
+	To : "kenzie@miadolce.com",
 	From : "<sender’s email address>",
 	Subject : "Test",
 	Body : "a Test",
@@ -10,3 +10,5 @@ function sendEmail() {
 		message => alert("Message Sent!")
 	);
 }
+
+console.log("this is good")
